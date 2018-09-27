aefb9289810db8efd96e202379e3c0d65284f827
JSnakegitHub <nyekowalter69@gmail.com>
2018-09-27 17:07:20 +0300

    Add tickets table

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ dbInstance = DatabaseConnectivity()
 dbInstance.create_users_table()
 dbInstance.create_clients_table()
 dbInstance.create_work_orders_table()
-dbInstance.create_tickets_table()
+# dbInstance.create_tickets_table()
 # dbInstance.create_engineers_table()
 
 if __name__ == '__main__':
