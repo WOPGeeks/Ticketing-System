251cd464006a5aafe9777e1cc3d420f27529531c
Walter Nyeko <walternyeko@MacBook-Pro-37.local>
2018-10-03 20:21:16 +0300

    Decorated the input fields

--- app/static/scripts/javascripts.js
+++ app/static/scripts/javascripts.js
@@ -5,3 +5,21 @@ function hidMessageDiv() {
         }, 10000);
     }
 }
+$(function(){
+    $( '.dropdown-menu li' ).on( 'click', function( event ) {
+        var $checkbox = $(this).find('.checkbox');
+        if (!$checkbox.length) {
+            return;
+        }
+        var $input = $checkbox.find('input');
+        var $icon = $checkbox.find('span.glyphicon');
+        if ($input.is(':checked')) {
+            $input.prop('checked',false);
+            $icon.removeClass('glyphicon-check').addClass('glyphicon-unchecked')
+        } else {
+            $input.prop('checked',true);
+            $icon.removeClass('glyphicon-unchecked').addClass('glyphicon-check')
+        }
+        return false;
+    }); 
+});
