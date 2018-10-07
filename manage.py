17723c15315ec50e1d8c655e23cb6f37a4b3f014
JSnakegitHub <nyekowalter69@gmail.com>
2018-10-07 13:37:08 +0300

    Remove users table

--- manage.py
+++ manage.py
@@ -2,11 +2,11 @@ from app.views import app
 from app.database.connectDB import DatabaseConnectivity
 
 dbInstance = DatabaseConnectivity()
-# dbInstance.create_users_table()
-# dbInstance.create_clients_table()
-# dbInstance.create_work_orders_table()
-# dbInstance.create_tickets_table()
-# dbInstance.create_engineers_table()
+dbInstance.create_users_table()
+dbInstance.create_clients_table()
+dbInstance.create_work_orders_table()
+dbInstance.create_tickets_table()
+dbInstance.create_engineers_table()
 
 if __name__ == '__main__':
     app.secret_key = 'mysecretkeyghjngdssdfghjhdfhghhsffdtrdddvdvbggdsewwessaae'
