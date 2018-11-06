0d2409143b2a95f9bea7cd34ef85872946878ad2
JSnakegitHub <nyekowalter69@gmail.com>
2018-11-06 11:22:56 +0300

    Deactivated db creations

--- manage.py
+++ manage.py
@@ -2,11 +2,11 @@ from app.views import app
 from app.database.connectDB import DatabaseConnectivity
 
 dbInstance = DatabaseConnectivity()
-dbInstance.create_users_table()
-dbInstance.create_clients_table()
-dbInstance.create_work_orders_table()
-dbInstance.create_tickets_table()
-dbInstance.create_equipments_table()
+# dbInstance.create_users_table()
+# dbInstance.create_clients_table()
+# dbInstance.create_work_orders_table()
+# dbInstance.create_tickets_table()
+# dbInstance.create_equipments_table()
 # dbInstance.drop_equipment_table()
 
 if __name__ == '__main__':
