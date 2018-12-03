8b6a43f04dd072d51f6deebbc5d0c64be9fe4e0f
Engjames256 <engjames256@gmail.com>
2018-12-03 21:47:35 +0300

    Fixed Job Card

--- app/users/users_model.py
+++ app/users/users_model.py
@@ -140,7 +140,7 @@ class Users:
         try:
             conn = dbInstance.connectToDatabase()
             cur = conn.cursor()
-            sql = """SELECT GROUP_CONCAT(user_email) FROM users WHERE user_can_receive_email_alerts=1"""
+            sql = """SELECT GROUP_CONCAT(user_email) FROM users WHERE user_can_receive_email_alertsc=1"""
             cur.execute(sql)
             self.theUsers = cur.fetchall()
             return self.theUsers
